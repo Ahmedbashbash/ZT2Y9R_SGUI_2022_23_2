8ed7e9f011f0d220832c5f99108d3debcb36e6cf
Ahmed Bashbash <Ahmadbashbash99@gmail.com>
2021-10-10 22:34:31 +0200

    created the project

---
+++ ZT2Y9R_HFT_2021221/ZT2Y9R_HFT_2021221.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ZT2Y9R_HFT_2021221.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ ZT2Y9R_HFT_2021221/ZT2Y9R_HFT_2021221.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ZT2Y9R_HFT_2021221.Models
+{
+    public class Class1
+    {
+    }
+}
