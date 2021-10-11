0c3c3d5de6225dab80c74f5ed8b88a5c5471f1bd
Ahmed Bashbash <Ahmadbashbash99@gmail.com>
2021-10-11 17:04:47 +0200

    ZT2Y9R_HFT_2021221 file update

---
+++ ZT2Y9R_HFT_2021221.Test/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ZT2Y9R_HFT_2021221.Test
+{
+    public class Class1
+    {
+    }
+}
--- ZT2Y9R_HFT_2021221/ZT2Y9R_HFT_2021221.Data/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace ZT2Y9R_HFT_2021221.Data
-{
-    //Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename="|DataDirectory|\Db.mdf";Integrated Security=True
-    public class Class1
-    {
-    }
-}
