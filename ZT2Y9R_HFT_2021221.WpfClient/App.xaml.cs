9225226f510de3b8442af25e0ea0c5eebd4905f0
Ahmed Bashbash <Ahmad.bashbash03@gmail.com>
2022-05-08 21:54:17 +0200

    WPF

---
+++ ZT2Y9R_HFT_2021221.WpfClient/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ZT2Y9R_HFT_2021221.WpfClient
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ ZT2Y9R_HFT_2021221.WpfClient/MainWindowViewModel.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using ZT2Y9R_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.ComponentModel;
+using System.Windows;
+using Microsoft.Toolkit.Mvvm.Input;
+
+namespace ZT2Y9R_HFT_2021221.WpfClient
+{
+    public class MainWindowViewModel : ObservableRecipient
+    {
+        private string errorMessage;
+
+        public string ErrorMessage
+        {
+            get { return errorMessage; }
+            set { SetProperty(ref errorMessage, value); }
+        }
+
+
+        public RestCollection<Players> Players { get; set; }
+
+        private Players selectedPlayer;
+
+        public Players SelectedPlayer
+        {
+            get { return selectedPlayer; }
+            set
+            {
+                if (value != null)
+                {
+                    selectedPlayer = new Players()
+                    {
+                        Name = value.Name,
+                        PlayerId = value.PlayerId
+                    };
+                    OnPropertyChanged();
+                    (DeletePlayerCommand as RelayCommand).NotifyCanExecuteChanged();
+                }
+            }
+        }
+
+
+        public ICommand CreatePlayerCommand { get; set; }
+
+        public ICommand DeletePlayerCommand { get; set; }
+
+        public ICommand UpdatePlayerCommand { get; set; }
+
+        public static bool IsInDesignMode
+        {
+            get
+            {
+                var prop = DesignerProperties.IsInDesignModeProperty;
+                return (bool)DependencyPropertyDescriptor.FromProperty(prop, typeof(FrameworkElement)).Metadata.DefaultValue;
+            }
+        }
+
+
+        public MainWindowViewModel()
+        {
+            if (!IsInDesignMode)
+            {
+                Players = new RestCollection<Players>("http://localhost:53910/", "actor", "hub");
+                CreatePlayerCommand = new RelayCommand(() =>
+                {
+                    Players.Add(new Players()
+                    {
+                        Name = SelectedPlayer.Name
+                    });
+                });
+
+                UpdatePlayerCommand = new RelayCommand(() =>
+                {
+                    try
+                    {
+                        Players.Update(SelectedPlayer);
+                    }
+                    catch (ArgumentException ex)
+                    {
+                        ErrorMessage = ex.Message;
+                    }
+
+                });
+
+                DeletePlayerCommand = new RelayCommand(() =>
+                {
+                    Players.Delete(SelectedPlayer.PlayerId);
+                },
+                () =>
+                {
+                    return SelectedPlayer != null;
+                });
+                SelectedPlayer = new Players();
+            }
+
+        }
+    }
+}
